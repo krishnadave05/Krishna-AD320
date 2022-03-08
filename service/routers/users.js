f76260758b72ffea10cb0954dc6d47f5f3491b90
krishnadave05 <krishnadave05@gmail.com>
2022-03-08 22:02:57 +0530

    Week9

---
+++ service/routers/users.js
@@ -0,0 +1,50 @@
+import { Router } from 'express'
+import { User } from '../models/User.js'
+
+const usersRouter = Router()
+
+const getUsers = async (req, res) => {
+  if(req.role == "Admin" || req.role == "SuperUser") {
+    const users = await User.find({})
+    res.send(users)
+  } else {
+    res.status(404).send('only admin and superuser can access this route')
+  }
+}
+
+const getUsersById = async (req, res) => {
+  if(req.role == "Admin" || req.role == "SuperUser") {
+    const user = await User.findById(req.params.id)
+  res.send(user)
+  } else {
+    res.status(404).send('only admin and superuser can access this route')
+  }
+}
+
+const updateUser = async (req, res) => {
+  if(req.role == "Admin" || req.userId == req.params.id) {
+    const result = await User.findByIdAndUpdate(req.params.id, req.body)
+    console.log('result ', result)
+    res.sendStatus(503)
+  } else {
+    res.status(404).send('only admin can access this route')
+  }
+}
+
+const deleteUser = async (req, res) => {
+  if(req.role == "Admin" || req.userId == req.params.id) {
+    const result = await User.findByIdAndUpdate(req.params.id, { active: false })
+    console.log('result ', result)
+    res.sendStatus(503)
+  } else {
+    res.status(404).send('only admin  can access this route')
+  }
+  
+}
+
+usersRouter.get('/', getUsers)
+usersRouter.get('/:id', getUsersById)
+usersRouter.put('/:id', updateUser)
+usersRouter.delete('/:id', deleteUser)
+
+export default usersRouter
